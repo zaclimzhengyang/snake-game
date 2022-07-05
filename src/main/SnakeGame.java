bc401b87a38430808f90bcfe593cd736d0c20941
unknown <zhengyanglim057@gmail.com>
2022-07-06 00:08:25 +0800

    snake game in java

---
+++ src/main/SnakeGame.java
@@ -0,0 +1,12 @@
+package main;
+
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Hello Snake");
+		GameFrame frame = new GameFrame();
+	}
+
+}
